21062866741e10d4a293f320988cc051dd1f66dd
Muhammad <ma.khan6971@gmail.com>
2024-09-21 22:36:50 +0100

    added images

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -38,6 +38,11 @@
                 class="rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-gray-700 hover:text-white"
                 >Visit</a
               >
+              <a
+                href="/roomexample"
+                class="rounded-md px-3 py-2 text-sm font-medium text-black hover:bg-gray-700 hover:text-white"
+                >booking</a
+              >
             </div>
           </div>
         </div>
